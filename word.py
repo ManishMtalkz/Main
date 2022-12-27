2dc558a67c57c6ebf6f2a09e379f67dbe06a3e73
Manish <manish.mtalkz@gmail.com>
2022-12-27 14:52:31 +0530

    Up

--- word.py
+++ word.py
@@ -17,18 +17,6 @@ def cleaning(text):
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
     text = re.sub('[’“”…]', '', text)     
-
-    # removing the emojies               # https://www.kaggle.com/alankritamishra/covid-19-tweet-sentiment-analysis#Sentiment-analysis
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
-    text = emoji_pattern.sub(r'', text)   
-    
     # removing the stop-words          
     text_tokens = word_tokenize(text)
     tokens_without_sw = [word for word in text_tokens if not word in stop_words]
